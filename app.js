450c8f320ad560dffe09fe242f45affd1c869d86
karl wernsman <wernsmank@gmail.com>
2022-09-20 11:58:31 -0700

    Player emotion capacity display functioning and picture change when at 0

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 /* Imports */
 
 /* Get DOM Elements */
+const playerCapacity = document.getElementById('player-emotional-capacity');
+const playerImg = document.getElementById('player-img');
 
 /* State */
+let player = {
+    ec: 20,
+    type: 'abigail',
+};
 
 /* Events */
 
 /* Display Functions */
-
+function displayPlayer() {
+    playerCapacity.textContent = Math.max(0, player.ec);
+    if (player.ec < 1) {
+        playerImg.src = 'assets/exhausted.png';
+    } else {
+        playerImg.src = `assets/${player.type}.png`;
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
+displayPlayer();
